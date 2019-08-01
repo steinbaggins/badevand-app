b8a6c09517ded79eb8944447858d7d5adf11800f
Mathias Kofoed <mrk@cmp-company.com>
2019-08-01 09:27:16 +0200

    Fetch api data, show in list & map

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { Feather, MaterialIcons } from '@expo/vector-icons';
+
+const flagColor = number => {
+  const colors = ['#e66767', '#3dc1d3', '#f5cd79', '#303952'];
+  return colors[number - 1];
+}
 
 export default function List() {
+  const [beaches, setBeaches] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('http://api.vandudsigten.dk/beaches');
+      const data = await res.json();
+      setBeaches(data)
+    }
+    fetchData();
+  },[]);
   return (
-    <View>
-      <Text>ListPage</Text>
-    </View>
+    <ScrollView>
+      <SafeAreaView>
+      {beaches.map(beach => (
+        <View key={ beach.id } style={ { flexDirection: 'row', alignItems: 'center' } }>
+          <MaterialIcons name="flag" size={ 50 } color={ flagColor(beach.data[0].water_quality) } />
+          <View>
+            <Text size={ 30 }>{ beach.name }</Text>
+            <View style={ { flexDirection: 'row', alignItems: 'center' } }>
+              <Feather name="droplet" size={ 25 } />
+              <Text size={ 30 }>{ beach.data[0].water_temperature }</Text>
+            </View>
+            <View style={ { flexDirection: 'row', alignItems: 'center' } }>
+              <Feather name="cloud-rain" size={ 25 } />
+              <Text size={ 30 }>{ beach.data[0].air_temperature }</Text>
+            </View>
+          </View>
+        </View>
+      ))}
+      </SafeAreaView>
+    </ScrollView>
   );
 }
--- src/containers/Map.js
+++ src/containers/Map.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+// import { Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const flagColor = number => {
+  const colors = ['#e66767', '#3dc1d3', '#f5cd79', '#303952'];
+  return colors[number - 1];
+}
 
 export default function Map() {
+  const [beaches, setBeaches] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('http://api.vandudsigten.dk/beaches');
+      const data = await res.json();
+      setBeaches(data)
+    }
+    fetchData();
+  },[]);
   return (
-    <View>
-      <Text>MapPage</Text>
-    </View>
+    <MapView
+      style={ { flex: 1 } }
+    >
+      {beaches.map(beach => (
+        <Marker key={ beach.id } coordinate={ { latitude: beach.latitude, longitude: beach.longitude } }>
+          <MaterialIcons name="flag" size={ 30 } color={ flagColor(beach.data[0].water_quality) } />
+        </Marker>
+      ))}
+    </MapView>
   );
 }
